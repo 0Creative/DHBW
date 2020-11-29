68ad7f2ba3b39eb8c7f4bb2943ed24d4abd43a92
0Creative <75220038+0Creative@users.noreply.github.com>
2020-11-29 20:27:40 +0100

    Add files via upload

---
+++ simulation/src/main/java/de/dhbw/simulation/SimulationApplication.java
@@ -0,0 +1,18 @@
+package de.dhbw.simulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulationApplication.class, args);
+	}
+
+
+
+
+
+
+}
---
+++ simulation/src/main/java/de/dhbw/simulation/dao/PersonDao.java
@@ -0,0 +1,28 @@
+package de.dhbw.simulation.dao;
+
+
+import de.dhbw.simulation.model.Person;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PersonDao {
+
+    int insertPerson(UUID id, Person person);
+
+    default int insertPerson(Person person) {
+        UUID id = UUID.randomUUID();
+        return insertPerson(id,person);
+
+    };
+
+    List<Person> selectAllPeople();
+
+    Optional<Person> selectPersonById(UUID id);
+
+    int deletePersonById(UUID id);
+
+    int updatePersonById(UUID id, Person person);
+
+}
